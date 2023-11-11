bf4931ebc80566fe03b561ba4f0f2ab1c0903716
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-11 17:13:44 +0000

    nested-fn-to-top-level: copy `radians` and `distance` fns to `top_calculateDistance`

--- src/examples/nested-fn-to-top-level/index.js
+++ src/examples/nested-fn-to-top-level/index.js
@@ -1,4 +1,22 @@
 function top_calculateDistance(points) {
+  function radians(degrees) {
+    return (degrees * Math.PI) / 180;
+  }
+
+  function distance(p1, p2) {
+    // haversine formula, see https://en.wikipedia.org/wiki/Haversine_formula
+    const EARTH_RADIUS_IN_KM = 6371;
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+    const a =
+      Math.sin(dLat / 2) ** 2 +
+      Math.cos(radians(p2.lat)) * Math.cos(radians(p1.lat)) * Math.sin(dLon / 2) ** 2;
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return Number((EARTH_RADIUS_IN_KM * c).toFixed(2));
+  }
+
   let result = 0;
   for (let i = 1; i < points.length; i += 1) {
     result += distance(points[i - 1], points[i]);
