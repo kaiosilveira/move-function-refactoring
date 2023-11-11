1561f6b2c3f5f95acb7376ea9b1f371a2354f474
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-11 17:42:15 +0000

    moving-between-classes: copy `overdraftCharge` to `AccountType`

--- src/examples/moving-between-classes/account-type/index.js
+++ src/examples/moving-between-classes/account-type/index.js
@@ -6,4 +6,14 @@ export default class AccountType {
   get isPremium() {
     return this.name === 'premium';
   }
+
+  overdraftCharge(daysOverdrawn) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+      if (daysOverdrawn <= 7) return baseCharge;
+      return baseCharge + (daysOverdrawn - 7) * 0.85;
+    }
+
+    return daysOverdrawn * 1.75;
+  }
 }
--- src/examples/moving-between-classes/account-type/index.test.js
+++ src/examples/moving-between-classes/account-type/index.test.js
@@ -1,15 +1,34 @@
 import AccountType from '.';
 
 describe('AccountType', () => {
+  const regularAccountType = new AccountType('regular');
+  const premiumAccountType = new AccountType('premium');
+
   describe('isPremium', () => {
     it('should return true if account name is "premium"', () => {
-      const type = new AccountType('premium');
-      expect(type.isPremium).toEqual(true);
+      expect(premiumAccountType.isPremium).toEqual(true);
     });
 
     it('should return false if account name is not "premium"', () => {
-      const type = new AccountType('regular');
-      expect(type.isPremium).toEqual(false);
+      expect(regularAccountType.isPremium).toEqual(false);
+    });
+  });
+
+  describe('overdraftCharge', () => {
+    describe('prmium accounts', () => {
+      it('should not charge for the first seven days on overdraft if it is a premium account', () => {
+        expect(premiumAccountType.overdraftCharge(7)).toEqual(10);
+      });
+
+      it('should charge a daily amount after the seven initial days on overdraft', () => {
+        expect(premiumAccountType.overdraftCharge(8)).toEqual(10.85);
+      });
+    });
+
+    describe('regular accounts', () => {
+      it('should charge the base overdraft charge plus a daily amount for each day in overdraft', () => {
+        expect(regularAccountType.overdraftCharge(1)).toEqual(1.75);
+      });
     });
   });
 });
